66b5e350475d11549fac3fdda13ea9b8aa6d30b8
MrKeeplearning <jaeseok1115@gmail.com>
2023-01-26 03:28:13 +0900

    programmers 42746 solution3 추가

--- MrKeeplearning/programmers/코딩테스트 고득점 Kit/정렬/42746(가장_큰_수).py
+++ MrKeeplearning/programmers/코딩테스트 고득점 Kit/정렬/42746(가장_큰_수).py
@@ -17,25 +17,69 @@ Solution1 -> 시간초과로 실패
 class Solution1:
     @staticmethod
     def to_swap(n1, n2):
-        return str(n1) + str(n2) < str(n2) + str(n1)
+        return n1 + n2 < n2 + n1
 
     def solution(self, numbers):
+        # 첫 번째 정렬
+        numbers_str = [str(num) for num in numbers]
         i = 1
-        while i < len(numbers):
+        while i < len(numbers_str):
             j = i
-            while j > 0 and self.to_swap(numbers[j - 1], numbers[j]):
-                numbers[j], numbers[j - 1] = numbers[j - 1], numbers[j]
+            while j > 0 and self.to_swap(numbers_str[j - 1], numbers_str[j]):
+                numbers_str[j], numbers_str[j - 1] = numbers_str[j - 1], numbers_str[j]
                 j -= 1
             i += 1
 
-        answer = str(int(''.join(map(str, numbers))))
+        answer = str(int(''.join(numbers_str)))
         return answer
+
+
+"""
+Solution2
+- 정렬을 두 번 해준다.
+- 첫 번째 정렬은 numbers에 담긴 요소를 str로 형 변환을 한 뒤 ascii 값을 기준으로 정렬을 한다.
+- 두 번째 정렬은 테스트 케이스로 공개한 [3, 30, 34, 5, 9] 때문에 적용한다.
+- 첫 번째 정렬로 한 번 정렬을 하면 [3, 30, 34, 5, 9] 은 [9, 5, 34, 30, 3]이 되는데, 이것을 그대로 이어붙여도 가장 큰 수가 아니다.
+- 두 번째 정렬에서 num을 3번 곱하는데 그 이유는 number의 제약조건으로 1000이하의 수만 들어간다고 되어 있었고
+  그래서 최대값을 생각해 세자리 수를 만들고 싶어 3을 곱했다고 한다.
+- 그렇게 해서 ascii 값으로 정렬을 하면 원하는 순서로 정렬된다고 하는데, 솔직히 납득이 잘 되지 않는다.
+- 너무 독특한 풀이 방법이고 이것을 내가 생각해 낼 수 있는 방법은 아닌 것 같다.
+"""
 
 
 class Solution2:
     @staticmethod
     def solution(numbers):
+        # 첫 번째 정렬
         numbers_str = [str(num) for num in numbers]
+        # 두 번째 정렬
         numbers_str.sort(key=lambda num: num * 3, reverse=True)
 
         return str(int(''.join(numbers_str)))
+
+
+"""
+Solution3
+- functools.cmp_to_key()를 활용한 풀이
+- solution2보다는 훨씬 현실적으로 생각해 볼 수 있을 법한 풀이인 것 같다.
+"""
+import functools
+
+
+class Solution3:
+
+    @staticmethod
+    def compare(n1, n2):
+        c1 = int(n1 + n2)
+        c2 = int(n2 + n1)
+        # c1 > c2 인 경우 양수를 반환
+        # c1 < c2 인 경우 음수를 반환
+        # c1 == c2 인 경우 0을 반환
+        return c1 - c2
+
+    @staticmethod
+    def solution(numbers):
+        nums_str = [str(x) for x in numbers]
+        sorted_nums = sorted(nums_str, key=functools.cmp_to_key(compare), reverse=True)
+        answer = str(int(''.join(sorted_nums)))
+        return answer
