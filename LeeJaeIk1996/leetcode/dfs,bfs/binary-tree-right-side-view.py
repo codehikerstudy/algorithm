b81ad8aa9552c59ff743a9cac6a9ca45cf6e74ef
LeeJaeIk1996 <ly7kdd@naver.com>
2023-03-30 22:38:58 +0900

    리트코드 199번 문제 주석 수정

--- LeeJaeIk1996/leetcode/dfs,bfs/binary-tree-right-side-view.py
+++ LeeJaeIk1996/leetcode/dfs,bfs/binary-tree-right-side-view.py
@@ -32,13 +32,14 @@ class Solution:
             if not node:
                 return
             
+            # 만약 현재의 깊이가 최대 깊이보다 깊을 경우 현재의 깊이에 있는 값을 answer 배열에 저장한다
             if depth > max_depth:
                 answer.append(node.val)
                 max_depth = depth
 
-            dfs(node.right, depth + 1)
-            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)  # 우측에서 보았을 때 숫자들이므로 right를 우선적으로 탐색한다
+            dfs(node.left, depth + 1)   # right를 전부 순회하였을 경우 left를 순회한다.
 
-        dfs(root, depth)
+        dfs(root, depth)    # root를 시작으로 dfs. 이 때, 깊이(depth)는 1부터
 
-        return answer
+        return answer   # 반환 값
