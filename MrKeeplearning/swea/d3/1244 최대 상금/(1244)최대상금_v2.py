0a295515246011873b6ab6d9ad402634e56cef4f
MrKeeplearning <jaeseok1115@gmail.com>
2023-05-21 02:34:07 +0900

    SWEA D3 1244 second attempt

--- MrKeeplearning/swea/d3/1244 최대 상금/(1244)최대상금_v2.py
+++ MrKeeplearning/swea/d3/1244 최대 상금/(1244)최대상금_v2.py
@@ -1,42 +1,35 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-T = int(input())
-answer = 0
-visited = []
+def dfs(count):
+    global max_value
 
-
-def dfs(n):
-    global answer
-    # 최대 스왑 가능한 횟수까지 도달했다면
-    # answer를 최대값으로 갱신한다.
-    if n == swap:
-        answer = max(answer, int("".join(lst)))
+    # 스왑 횟수를 모두 사용한 경우
+    if not count:
+        tmp = int(''.join(values))
+        if tmp > max_value:
+            max_value = tmp
         return
 
-    # length개에서 2개를 뽑는 모든 조합
-    # 뽑은 인덱스 값을 서로 swap한다.
-    for i in range(length-1):
+    for i in range(length):
         for j in range(i+1, length):
-            lst[i], lst[j] = lst[j], lst[i]     # 스왑
+            values[i], values[j] = values[j], values[i]
+            temp_key = ''.join(values)
+            if visited.get((temp_key, count-1), 1):
+                visited[(temp_key, count-1)] = 0
+                dfs(count - 1)
+            values[i], values[j] = values[j], values[i]
 
-            # 스왑한 결과물이 이전에 나오지 않았다면 DFS탐색을 계속하고
-            # 이전에 이미 만났던 결과물이면 넘어간다.
-            check = int("".join(lst))*10 + n
-            if check not in visited:
-                dfs(n+1)
-                visited.append(check)
 
-            # 원상 복구를 해야 처음 인덱스 순서를 유지한 상태에서 조합을 구할 수 있다.
-            lst[i], lst[j] = lst[j], lst[i]
-
+T = int(input())
 
 for test_case in range(1, T + 1):
-    nums, s = input().split()
+    max_value = 0
+    num, s = input().split()
+    values = list(num)
     swap = int(s)
-    lst = []
-    for num in nums:
-        lst.append(num)
-    length = len(lst)
-    dfs(0)
-    print(f'#{test_case} {answer}')
+    length = len(values)
+    visited = {}
+    dfs(swap)
+
+    print(f'#{test_case} {max_value}')
